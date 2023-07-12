a347aa108a44dc0284958a1a12901965c8ae1fbc
Li <jingnanl@b0be83742b8f.ant.amazon.com>
2023-07-12 11:52:28 +0800

    Initial commit

---
+++ test/lambda-extension-s3-logs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaExtensionS3Logs from '../lib/lambda-extension-s3-logs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-extension-s3-logs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaExtensionS3Logs.LambdaExtensionS3LogsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
